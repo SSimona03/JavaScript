d86a110bad68c872121a294f3d4bea8d4e053a9b
ssimona03 <sstroe92@gmail.com>
2022-11-01 16:08:26 +0000

    swich

--- js-swipe-light-dark-theme/script.js
+++ js-swipe-light-dark-theme/script.js
@@ -3,6 +3,6 @@ let rotate = document.querySelector('.icon')
 
 document.querySelector(".main-button").addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    //rotate.style.setProperty(0, 180);
+    rotate.style.setProperty(0, 180);
     
 })
