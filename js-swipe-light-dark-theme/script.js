635f6116bac4007cb2b6c526af498b1ff317ae58
ssimona03 <sstroe92@gmail.com>
2022-11-01 13:38:19 +0000

    add project js swipe light dark theme

---
+++ js-swipe-light-dark-theme/script.js
@@ -0,0 +1,8 @@
+
+let rotate = document.querySelector('.icon')
+
+document.querySelector(".main-button").addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    //rotate.style.setProperty(0, 180);
+    
+})
